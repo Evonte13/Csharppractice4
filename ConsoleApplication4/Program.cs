d7c6a8f73f8f686c9eee14a549587aed69c88e56
evonte dunn <evontedunn@gmail.com>
2017-06-02 20:26:45 -0400

    first commit

---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int money = 24;
+            int pencilsBought = 7;
+            int cashBack = 10;
+            int pencilsCost = (money - cashBack) / pencilsBought;
+
+            Console.WriteLine(pencilsCost);
+            
+
+        }
+
+        
+
+    }
+}
